46d26bca04076c82526fae4b1760b7e54aea6a27
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-12-27 12:58:27 -0800

    Move event handler outside of compenent

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import Button from './Button'
 import { Component } from 'react';
 
 class App extends React.Component {
+  buttonClicked(){
+    alert('You clicked on the button!')
+  }
   render(){
   return (
     <div>
       <h1>{this.props.appName}</h1>
-      <Button title='Click me!'/>
+      <Button title='Click me!' click={this.buttonClicked}/>
     </div>
     
   );
--- src/Button.js
+++ src/Button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 class Button extends React.Component{
-   buttonClicked(){
-    alert('You clicked on the button!')
-  }
+  // buttonClicked(){
+  //   alert('You clicked on the button!')
+  // }
 
  render(){
-   return <button onClick={this.buttonClicked}>{this.props.title}</button>
+   return <button onClick={this.props.click}>{this.props.title}</button>
  }
 }
 export default Button;
