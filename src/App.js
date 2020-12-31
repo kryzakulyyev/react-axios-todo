59d921f826741d46350cfc6e5532202ceaed9ebc
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-12-30 17:14:33 -0800

    Added some staff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import Button from './Button'
 import { Component } from 'react';
 
@@ -16,28 +16,41 @@ import { Component } from 'react';
 //   );
 //   }
 // }
-function randomNumber(){
-  return Math.floor(Math.random() * 255) + 1;
-}
 
-function buttonClicked(){
-  const redColor = randomNumber();
-  const greenColor = randomNumber();
-  const blueColor = randomNumber();
-  const rgbColor= `rgb(${redColor},${greenColor},${blueColor})`; 
-  document.getElementById('header').style.color=rgbColor;
-  document.getElementById('header').innerText=rgbColor;
-}
 
-function App(props) {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      color: 'click color'
+    }
+    this.buttonClicked = this.buttonClicked.bind(this)
+  }
+  randomNumber(){
+    return Math.floor(Math.random() * 255) + 1;
+  }
   
+   buttonClicked(){
+    const redColor = this.randomNumber();
+    const greenColor = this.randomNumber();
+    const blueColor = this.randomNumber();
+    const rgbColor= `rgb(${redColor},${greenColor},${blueColor})`; 
+    // document.getElementById('header').style.color=rgbColor;
+    // document.getElementById('header').innerText=rgbColor;
+    this.setState({color: rgbColor})
+  }
+  render(){
+  let headerStyle= {
+    color: this.state.color
+  }
   return (
     <div>
-      <h1 id='header'>{props.appName}</h1>
-      <Button title='Click me!' onClick={buttonClicked}/>
+      <h1 id='header'  style={{color: this.state.color}}>{this.state.color}</h1>
+      <Button title='Click me!' onClick={this.buttonClicked}/>
     </div>
     
   );
+  }
   }
 
 
