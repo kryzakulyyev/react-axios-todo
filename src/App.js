bebdebf94582403eddfcd284d414d09acbb7a4b1
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-12-30 17:20:24 -0800

    Remove Componenst to function

--- src/App.js
+++ src/App.js
@@ -18,40 +18,18 @@ import { Component } from 'react';
 // }
 
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      color: 'click color'
-    }
-    this.buttonClicked = this.buttonClicked.bind(this)
-  }
-  randomNumber(){
-    return Math.floor(Math.random() * 255) + 1;
-  }
-  
-   buttonClicked(){
-    const redColor = this.randomNumber();
-    const greenColor = this.randomNumber();
-    const blueColor = this.randomNumber();
-    const rgbColor= `rgb(${redColor},${greenColor},${blueColor})`; 
-    // document.getElementById('header').style.color=rgbColor;
-    // document.getElementById('header').innerText=rgbColor;
-    this.setState({color: rgbColor})
-  }
-  render(){
-  let headerStyle= {
-    color: this.state.color
-  }
+function App  {
+ 
+
   return (
     <div>
-      <h1 id='header'  style={{color: this.state.color}}>{this.state.color}</h1>
-      <Button title='Click me!' onClick={this.buttonClicked}/>
+      <h1> Todo List</h1>
+     
     </div>
     
   );
   }
-  }
+
 
 
 export default App;
