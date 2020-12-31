8ae42debb479f198c30f447a924c5a6ea6ef70f1
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2020-12-30 17:49:30 -0800

    Create TodoList component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React from 'react'
-import Button from './Button'
-import { Component } from 'react';
-
-// class App extends React.Component {
-//   buttonClicked(){
-//     alert('You clicked on the button!')
-//   }
-//   render(){
-//   return (
-//     <div>
-//       <h1>{this.props.appName}</h1>
-//       <Button title='Click me!' click={this.buttonClicked}/>
-//     </div>
-    
-//   );
-//   }
-// }
-
-
-function App  {
- 
-
+import TodoList from './components/TodoList'
+function App()  {
   return (
     <div>
-      <h1> Todo List</h1>
-     
-    </div>
-    
-  );
+      <h1>Todo List</h1>
+      <TodoList/>
+   </div>
+    );
   }
 
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class TodoList extends React.Component{
+ 
+  render(){
+    return (
+    <form>
+      <input type='text'/>
+      <button type='button'>Add</button>
+    </form>
+    )
+  }
+}
+export default TodoList;
